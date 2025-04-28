afd98da608a46ff5880fa2646e13824f4a3cf3fa
Yannick <huyiyan1986@gmail.com>
2025-04-28 09:33:39 +0200

    init commit

---
+++ back/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
